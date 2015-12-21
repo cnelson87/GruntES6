b5309b6bf7e440a2680ae4d67454c34ee918c1ac
Chris <cnelson87@gmail.com>
2015-12-21 14:09:16 -0800

    update libs and plugins

--- grunt_tasks/bower.js
+++ grunt_tasks/bower.js
@@ -12,7 +12,25 @@ module.exports = function (grunt) {
 			options: {
 				stripJsAffix: true,
 				keepExpandedHierarchy: false,
-				expand: false
+				expand: false,
+				packageSpecific: {
+					greensock: {
+						dest: '<%= vendorPath %>/greensock',
+						files: [
+							'src/minified/TweenMax.min.js',
+							'src/minified/plugins/ScrollToPlugin.min.js'
+						]
+					},
+					ScrollMagic: {
+						dest: '<%= vendorPath %>/scrollmagic',
+						files: [
+							'scrollmagic/minified/ScrollMagic.min.js',
+							'scrollmagic/minified/plugins/jquery.ScrollMagic.min.js',
+							'scrollmagic/minified/plugins/animation.gsap.min.js',
+							'scrollmagic/minified/plugins/debug.addIndicators.min.js'
+						]
+					}
+				}
 			},
 			dest: '<%= sourceVendor %>'
 		}
