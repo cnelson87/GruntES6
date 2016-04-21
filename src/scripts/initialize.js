4cd1b32493552f02ad3a71f79ae515ddbe193c24
Chris Nelson <Chris.Nelson@popagency.com>
2016-04-21 11:59:09 -0700

    add handlebars helpers

--- src/scripts/initialize.js
+++ src/scripts/initialize.js
@@ -3,8 +3,10 @@
  * @author Chris Nelson <cnelson87@gmail.com>
  */
 
-import Application from './Application';
+import HandlebarsHelpers from 'config/HandlebarsHelpers';
+import Application from './Application.js';
 
 $(function() {
+	new HandlebarsHelpers();
 	Application.initialize();
 });
