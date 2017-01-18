9fdf050962f8703c4863e082321f27c0b8211eb3
Chris Nelson <Chris.Nelson@popagency.com>
2017-01-17 17:42:36 -0800

    JS config updates

--- src/scripts/config/AppConfig.js
+++ src/scripts/config/AppConfig.js
@@ -4,7 +4,7 @@
  */
 
 if (!window.location.origin) {
-	window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
+	window.location.origin = window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
 }
 
 const AppConfig = {
@@ -12,15 +12,11 @@ const AppConfig = {
 	siteUrl: window.location.origin,
 	isIE9: navigator.userAgent.indexOf('MSIE 9') !== -1,
 	isIE10: navigator.userAgent.indexOf('MSIE 10') !== -1,
-	isIE11: navigator.userAgent.indexOf('MSIE 11') !== -1,
-
+	isIE11: (navigator.userAgent.indexOf('Windows NT') !== -1 && navigator.userAgent.indexOf('rv:11') !== -1),
 	isAndroid: /(android)/i.test(navigator.userAgent),
 	isIOS: navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false,
-
 	hasFormValidation: typeof document.createElement('input').checkValidity === 'function',
-	hasTouch: !!('ontouchstart' in window),
-
-	topOffset : 0,
+	hasTouch: Boolean('ontouchstart' in window || navigator.maxTouchPoints || navigator.msMaxTouchPoints),
 
 	urls: {
 		fibonacci: '/_api/promises/fibonacci.json',
@@ -45,7 +41,9 @@ const AppConfig = {
 		1: 'mobile',
 		2: 'tablet',
 		3: 'desktop'
-	}
+	},
+
+	topOffset : 0
 
 };
 
